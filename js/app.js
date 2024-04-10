8ae994ee49c7ada4fb65b802c0883e6d3cbc4a13
Dan Collins <dan.collins.dev@gmail.com>
2024-04-10 15:56:07 -0400

    removed style rendering until I find out how to render slots as they should based on matches

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ const submitBtn = document.getElementById("submit-btn");
 const clearBtn = document.getElementById("clear-btn");
 let currentSlotIdx = 0;
 const counts = {};
-const onMatch = new CustomEvent("app:onMatch");
-const renderSlots = new CustomEvent("app:renderSlots", {detail: {counts}});
 
 let guess = [];
 const answer = [];
@@ -28,97 +26,26 @@ numBtns.forEach((element, idx, arr) => {
     });
 });
 
-// document.addEventListener("app:onMatch", () => {
-//     if (guess.length === answer.length) {
-//         submitBtn.disabled = false;
-//     } else {
-//         submitBtn.disabled = true;
-//     }
-// });
-
 submitBtn.disabled = true;
 
-// Contains the logic for matching guess against answer
-// as well removing invalid
+
 const matchSlots = (e) => {
     let currentCounts = structuredClone(counts);
     submitBtn.disabled = true;
-    let matchObj = new Map();
-    // let colorMatches = [];
+    let matchMap = new Map();
 
     slots.forEach((element, idx, arr) => {
         console.log(`Anser Index Value: ${answer[idx]}`)
         if (answer[idx] === +guess[idx]) {
-            currentCounts[]
-            matchObj.set(idx, {slot: element, correct: true, correctPosition: true, guess: +guess[idx], answer: answer[idx], numCounts: 0});
+            matchMap.set(idx, {slot: element, correct: true, correctPosition: true});
         } else if (answer.includes(+guess[idx]) && answer[idx] !== +guess[idx]) {
-            matchObj.set(idx, {slot: element, correct: true, correctPosition: false, guess: +guess[idx], answer: answer[idx], numCounts: 0});
+            matchMap.set(idx, {slot: element, correct: true, correctPosition: false});
         } else {
-            matchObj.set(idx, {slot: element, correct: false, correctPosition: false, guess: +guess[idx], answer: answer[idx], numCounts: 0});
+            matchMap.set(idx, {slot: element, correct: false, correctPosition: false});
         }
     });
-    console.log(matchObj)
-
-    // style corrects
-    matchObj.forEach((element) => {
-        console.log(element);
-    })
-
-    let incorrectMap = new Map()
-    // Style slots
-    matchObj.forEach((element, idx, arr) => {
-        // console.log(`Value of Answer IDX: ${answer[idx]}`)
-        console.log(`Element num counts: ${element.numCounts}`)
-
-        if (element.correct === false) {
-            element.slot.classList.add("wrong-color")
-        }
-
-        if (element.correct === true && element.correctPosition === false) {
-            element.numCounts += 1;
-            if (element.numCounts > counts[idx]) {
-                element.slot.classList.add("wrong-color")
-            }
-            else {
-
-                element.slot.classList.add("includes-color");
-            }
-        }
-
-        if (element.correct === true && element.correctPosition === true) {
-            element.numCounts += 1
-            
-             element.slot.classList.add("correct-color")
-            // if () {
-            //     element.slot.classList.add("correct-color")
-
-            // }
-            // else {
-            //     element.slot.classList.add("includes-color")
-            // }
-            // element.numCounts -= 1
-        
-        }
-        else if (element.correct == true && element.correctPosition === false) {
-            element.numCounts += 1;
-            if (element.numCounts <= currentCounts[answer[idx]]) {
-                element.slot.classList.add("includes-color")
-            } else {
-                element.slot.classList.add("wrong-color")
-            }
-        }
-
-        else {
-            element.slot.classList.add("wrong-color")
-        }
-    });
-    // console.log(incorrectMap)
+    console.log(matchMap)
 };
-
-const updateStyles = (arr) => {
-    console.log(arr)
-}
-
 
 submitBtn.addEventListener("click", matchSlots);
 
